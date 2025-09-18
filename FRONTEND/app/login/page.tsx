4daf6d85649507e9476d993038820a01f965dd4a
samumunoz <samumunoz@unal.edu.co>
2025-09-18 18:19:48 -0500

    add:añadí que no haya scroll, redondíe el logo de la UNAL (ME AYUDO SESEGA)

--- FRONTEND/app/login/page.tsx
+++ FRONTEND/app/login/page.tsx
@@ -12,19 +12,19 @@ export default function LoginPage() {
         <div className="bg-gradient-to-br from-green-400 to-green-10000000 
                     dark:from-green-600 dark:to-green-10000000 
                     unal:from-green-600/90 unal:to-green-800/90 
-                    p-8 rounded-3xl shadow-2xl 
+                    p-8 px-8 rounded-3xl shadow-2xl 
                     dark:shadow-green-900/30 unal:shadow-green-900/50 
-                    max-w-sm w-full mx-auto flex gap-8 items-center backdrop-blur-sm">
+                    max-w-100 w-full mx-auto flex gap-8 items-center backdrop-blur-sm">
           {/* Left side - Login Form */}
           <div className="flex-1">
             <div className="flex items-center gap-3 mb-8">
                   <div>
-              <div className="  flex items-center justify-center margin" >
+              <div className="  flex items-center justify-center" >
                 <div className="text-accent-foreground font-bold">
                   <img 
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeuoYIDwzHH-uI2m6Zsc5q-WNzZ-p8h9-8XQ&s " 
                     alt="Logo" 
-                    className="w-32 h-32 object-cover rounded-lg"
+                    className="w-32 h-32 object-cover rounded-full my-3"
                   />
                 </div>
               </div>
@@ -41,7 +41,7 @@ export default function LoginPage() {
       </div>
 
       {/* Footer */}
-      <footer className="p-6 text-center text-sm text-gray-600 dark:text-gray-400">
+      <footer className="p-0 text-center text-sm text-gray-600 dark:text-gray-400">
         © 2025 DebatIA. Todos los derechos reservados.
       </footer>
     </div>
