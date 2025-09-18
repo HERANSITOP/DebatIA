0b8fe1a11f89f15ff08606712cb94d66d3a09139
samumunoz <samumunoz@unal.edu.co>
2025-09-18 15:38:38 -0500

    feat: Modificamos los colores de la tarjeta, el tamaño, el logo de la universidad

--- FRONTEND/app/login/page.tsx
+++ FRONTEND/app/login/page.tsx
@@ -9,26 +9,26 @@ export default function LoginPage() {
                  before:content-[''] before:absolute before:inset-0 unal:before:bg-black/30">
       <ThemeToggleButton />
       <div className="flex-1 flex items-center justify-center px-4 py-12">
-        <div className="bg-gradient-to-br from-green-400 to-green-600 
-                    dark:from-green-600 dark:to-green-800 
+        <div className="bg-gradient-to-br from-green-400 to-green-10000000 
+                    dark:from-green-600 dark:to-green-10000000 
                     unal:from-green-600/90 unal:to-green-800/90 
                     p-8 rounded-3xl shadow-2xl 
                     dark:shadow-green-900/30 unal:shadow-green-900/50 
-                    max-w-6xl w-full mx-auto flex gap-8 items-center backdrop-blur-sm">
+                    max-w-sm w-full mx-auto flex gap-8 items-center backdrop-blur-sm">
           {/* Left side - Login Form */}
           <div className="flex-1">
             <div className="flex items-center gap-3 mb-8">
-              <div className="w-12 h-12 bg-white rounded-xl flex items-center justify-center shadow-md">
-                <span className="text-accent-foreground font-bold">
+                  <div>
+              <div className="  flex items-center justify-center margin" >
+                <div className="text-accent-foreground font-bold">
                   <img 
-                    src="https://purina.com.pe/sites/default/files/2022-10/Que_debes_saber_antes_de_adoptar_un_gatito.jpg" 
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeuoYIDwzHH-uI2m6Zsc5q-WNzZ-p8h9-8XQ&s " 
                     alt="Logo" 
-                    className="w-10 h-10 object-cover rounded-lg"
+                    className="w-32 h-32 object-cover rounded-lg"
                   />
-                </span>
+                </div>
               </div>
-              <div>
-                <h1 className="text-2xl font-bold text-white">DebatIA</h1>
+                <h1 className="text-2xl font-bold text-white text-center">DebatIA</h1>
                 <p className="text-green-50">Accede a tu cuenta con el correo de la universidad</p>
               </div>
             </div>
@@ -36,13 +36,7 @@ export default function LoginPage() {
           </div>
 
           {/* Right side - Illustration/Background */}
-          <div className="flex-1 hidden lg:block">
-            <img 
-              src="/background-img.png" 
-              alt="Background illustration" 
-              className="w-full h-auto object-contain max-h-[500px]"
-            />
-          </div>
+          
         </div>
       </div>
 
